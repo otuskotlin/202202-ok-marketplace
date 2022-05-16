e468951de10464c3545b81d2b794fdbe9d7ed7d6
BarracudaPff <mrm.kikll@gmail.com>
2022-05-16 12:01:08 +0300

    add Ktor native server app

---
+++ ok-marketplace-app-ktor-native/ README.md
@@ -0,0 +1,10 @@
+# embeddedServer - Native
+
+A copy of `ok-marketplace-app-ktor`, but with Kotlin Native realisation
+
+## Running
+
+To run a sample, execute the following command in a repository's root directory:
+```bash
+./gradlew :ok-marketplace-app-ktor-native:runReleaseExecutableNative
+```
---
+++ ok-marketplace-app-ktor-native/build.gradle.kts
@@ -0,0 +1,56 @@
+val ktorVersion: String by project
+
+plugins {
+    application
+    kotlin("multiplatform")
+}
+
+repositories {
+    mavenCentral()
+    maven { url = uri("https://maven.pkg.jetbrains.space/public/p/ktor/eap") }
+}
+
+kotlin {
+    val hostOs = System.getProperty("os.name")
+    val nativeTarget = when {
+        hostOs == "Mac OS X" -> macosX64("native")
+        hostOs == "Linux" -> linuxX64("native")
+        // Windows is currently not supported
+        // Other supported targets are listed here: https://ktor.io/docs/native-server.html#targets
+        else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
+    }
+
+    nativeTarget.apply {
+        binaries {
+            executable {
+                entryPoint = "ru.otus.otuskotlin.marketplace.main"
+            }
+        }
+    }
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation(project(":ok-marketplace-common"))
+                implementation(project(":ok-marketplace-api-v2-kmp"))
+                implementation(project(":ok-marketplace-mappers-v2"))
+
+                implementation(project(":ok-marketplace-services"))
+                implementation(project(":ok-marketplace-stubs"))
+            }
+        }
+        val nativeMain by getting {
+            dependencies {
+                implementation("io.ktor:ktor-server-core:$ktorVersion")
+                implementation("io.ktor:ktor-server-cio:$ktorVersion")
+
+                implementation("io.ktor:ktor-server-content-negotiation:$ktorVersion")
+                implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
+            }
+        }
+        val nativeTest by getting {
+            dependencies {
+                implementation(kotlin("test"))
+            }
+        }
+    }
+}
