473c31a3fc756c993ba3082c39c3313d07146cf2
Sergey Okatov <sokatov@gmail.com>
2022-05-29 19:52:56 +0500

    M3L2-s3 Интеграция с Spring

--- ok-marketplace-app-spring/src/main/kotlin/ru/otus/otuskotlin/marketplace/springapp/api/v1/controller/AdController.kt
+++ ok-marketplace-app-spring/src/main/kotlin/ru/otus/otuskotlin/marketplace/springapp/api/v1/controller/AdController.kt
@@ -1,11 +1,12 @@
 package ru.otus.otuskotlin.marketplace.springapp.api.v1.controller
 
+import kotlinx.coroutines.runBlocking
+import kotlinx.datetime.Clock
 import org.springframework.web.bind.annotation.*
 import ru.otus.otuskotlin.marketplace.api.v1.models.*
 import ru.otus.otuskotlin.marketplace.backend.services.AdService
 import ru.otus.otuskotlin.marketplace.common.MkplContext
 import ru.otus.otuskotlin.marketplace.mappers.v1.*
-import ru.otus.otuskotlin.marketplace.springapp.api.v1.buildError
 
 @RestController
 @RequestMapping("v1/ad")
@@ -14,41 +15,53 @@ class AdController(
 ) {
 
     @PostMapping("create")
-    fun createAd(@RequestBody createAdRequest: AdCreateRequest): AdCreateResponse {
-        val context = MkplContext().apply { fromTransport(createAdRequest) }
-
-        return adService.createAd(context).toTransportCreate()
+    fun createAd(@RequestBody request: AdCreateRequest): AdCreateResponse {
+        val ctx = MkplContext(
+            timeStart = Clock.System.now(),
+        )
+        ctx.fromTransport(request)
+        runBlocking { adService.createAd(ctx) }
+        return ctx.toTransportCreate()
     }
 
     @PostMapping("read")
-    fun readAd(@RequestBody readAdRequest: AdReadRequest) =
-        MkplContext().apply {
-            fromTransport(readAdRequest)
-        }.let {
-            adService.readAd(it, ::buildError)
-        }.toTransportRead()
+    fun readAd(@RequestBody request: AdReadRequest): AdReadResponse {
+        val ctx = MkplContext(
+            timeStart = Clock.System.now(),
+        )
+        ctx.fromTransport(request)
+        runBlocking { adService.readAd(ctx) }
+        return ctx.toTransportRead()
+
+    }
 
     @RequestMapping("update", method = [RequestMethod.POST])
-    fun updateAd(@RequestBody updateAdRequest: AdUpdateRequest): AdUpdateResponse {
-        return MkplContext().apply {
-            fromTransport(updateAdRequest)
-        }.let {
-            adService.updateAd(it, ::buildError)
-        }.toTransportUpdate()
+    fun updateAd(@RequestBody request: AdUpdateRequest): AdUpdateResponse {
+        val ctx = MkplContext(
+            timeStart = Clock.System.now(),
+        )
+        ctx.fromTransport(request)
+        runBlocking { adService.updateAd(ctx) }
+        return ctx.toTransportUpdate()
     }
 
     @PostMapping("delete")
-    fun deleteAd(@RequestBody deleteAdRequest: AdDeleteRequest): AdDeleteResponse {
-        val context = MkplContext().apply { fromTransport(deleteAdRequest) }
-
-        val result = adService.deleteAd(context, ::buildError)
-
-        return result.toTransportDelete()
+    fun deleteAd(@RequestBody request: AdDeleteRequest): AdDeleteResponse {
+        val ctx = MkplContext(
+            timeStart = Clock.System.now(),
+        )
+        ctx.fromTransport(request)
+        runBlocking { adService.deleteAd(ctx) }
+        return ctx.toTransportDelete()
     }
 
     @PostMapping("search")
-    fun searchAd(@RequestBody searchAdRequest: AdSearchRequest) =
-        MkplContext().apply { fromTransport(searchAdRequest) }.let {
-            adService.searchAd(it, ::buildError)
-        }.toTransportSearch()
+    fun searchAd(@RequestBody request: AdSearchRequest): AdSearchResponse {
+        val ctx = MkplContext(
+            timeStart = Clock.System.now(),
+        )
+        ctx.fromTransport(request)
+        runBlocking { adService.searchAd(ctx) }
+        return ctx.toTransportSearch()
+    }
 }
--- ok-marketplace-app-spring/src/main/kotlin/ru/otus/otuskotlin/marketplace/springapp/configs/ServiceConfig.kt
+++ ok-marketplace-app-spring/src/main/kotlin/ru/otus/otuskotlin/marketplace/springapp/configs/ServiceConfig.kt
@@ -3,13 +3,9 @@ package ru.otus.otuskotlin.marketplace.springapp.configs
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import ru.otus.otuskotlin.marketplace.backend.services.AdService
-import ru.otus.otuskotlin.marketplace.backend.services.OfferService
 
 @Configuration
 class ServiceConfig {
     @Bean
     fun serviceAd(): AdService = AdService()
-
-    @Bean
-    fun serviceOffer(): OfferService = OfferService()
 }
