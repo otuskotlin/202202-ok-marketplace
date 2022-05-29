a7f98f849235788aaf99d5a8978aaf6d375abc8d
Sergey Okatov <sokatov@gmail.com>
2022-05-29 10:25:13 +0500

    M3L2-s0 Инициализация модуля бизнес-логики

--- ok-marketplace-biz/build.gradle.kts
+++ ok-marketplace-biz/build.gradle.kts
@@ -2,24 +2,21 @@ plugins {
     kotlin("multiplatform")
 }
 
-
+repositories {
+    mavenCentral()
+}
 
 kotlin {
     jvm {}
-    macosX64 {}
-    linuxX64 {}
 
     sourceSets {
-        val datetimeVersion: String by project
-        val coroutinesVersion: String by project
 
         @Suppress("UNUSED_VARIABLE")
         val commonMain by getting {
             dependencies {
                 implementation(kotlin("stdlib-common"))
 
-                api("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
-                api("org.jetbrains.kotlinx:kotlinx-datetime:$datetimeVersion")
+                implementation(project(":ok-marketplace-common"))
             }
         }
         @Suppress("UNUSED_VARIABLE")
