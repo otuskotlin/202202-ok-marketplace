6c5679f0be0f5eaeb87a8bfc759ac9eb333fb9a8
DTadescu <tada.mascot@gmail.com>
2022-06-17 22:38:31 +0300

    M6L1 Перенес абстрактные тесты, пока без offers

---
+++ ok-marketplace-repo-test/src/main/kotlin/ru/otus/otuskotlin/marketplace/backend/repo/test/RepoAdOffersTest.kt
@@ -0,0 +1,13 @@
+package ru.otus.otuskotlin.marketplace.backend.repo.test
+
+import kotlinx.coroutines.runBlocking
+import org.junit.Test
+import ru.otus.otuskotlin.marketplace.common.models.MkplAd
+import ru.otus.otuskotlin.marketplace.common.models.MkplDealSide
+import ru.otus.otuskotlin.marketplace.common.models.MkplUserId
+import ru.otus.otuskotlin.marketplace.common.repo.DbAdFilterRequest
+import ru.otus.otuskotlin.marketplace.common.repo.IAdRepository
+import kotlin.test.assertEquals
+
+abstract class RepoAdOffersTest {
+}
