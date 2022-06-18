a3cf9d82ba2ce38f8d80d69102e5b83b6d42ad42
DTadescu <tada.mascot@gmail.com>
2022-06-18 22:44:22 +0300

    M6L1 Тест для offers

--- ok-marketplace-repo-test/src/main/kotlin/ru/otus/otuskotlin/marketplace/backend/repo/test/RepoAdOffersTest.kt
+++ ok-marketplace-repo-test/src/main/kotlin/ru/otus/otuskotlin/marketplace/backend/repo/test/RepoAdOffersTest.kt
@@ -1,13 +1,35 @@
 package ru.otus.otuskotlin.marketplace.backend.repo.test
 
 import kotlinx.coroutines.runBlocking
-import org.junit.Test
 import ru.otus.otuskotlin.marketplace.common.models.MkplAd
 import ru.otus.otuskotlin.marketplace.common.models.MkplDealSide
-import ru.otus.otuskotlin.marketplace.common.models.MkplUserId
-import ru.otus.otuskotlin.marketplace.common.repo.DbAdFilterRequest
+import ru.otus.otuskotlin.marketplace.common.repo.DbAdIdRequest
 import ru.otus.otuskotlin.marketplace.common.repo.IAdRepository
+import kotlin.test.Test
 import kotlin.test.assertEquals
 
 abstract class RepoAdOffersTest {
+    abstract val repo: IAdRepository
+
+    @Test
+    fun searchByTitle() {
+        val result = runBlocking { repo.searchOffers(DbAdIdRequest(searchId)) }
+        assertEquals(true, result.isSuccess)
+        assertEquals(2, result.result?.size)
+        result.result?.forEach {
+            assertEquals(MkplDealSide.SUPPLY, it.adType)
+            assert(it.title.contains(searchSuf))
+        }
+    }
+
+    companion object : BaseInitAds("search") {
+        val searchSuf = "ad1"
+        override val initObjects: List<MkplAd> = listOf(
+            createInitTestModel(suf = searchSuf, adType = MkplDealSide.DEMAND),
+            createInitTestModel(suf = searchSuf, adType = MkplDealSide.SUPPLY),
+            createInitTestModel(suf = searchSuf, adType = MkplDealSide.SUPPLY)
+        )
+        val searchId = initObjects.first().id
+
+    }
 }
