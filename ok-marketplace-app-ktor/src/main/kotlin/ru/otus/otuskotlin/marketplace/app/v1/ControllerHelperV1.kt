dc21db6ab45b852e3a96f80e556db9ee25ce71e8
DTadescu <tada.mascot@gmail.com>
2022-08-02 21:07:29 +0300

    M8L2 Логирование в приложении ktor v1

--- ok-marketplace-app-ktor/src/main/kotlin/ru/otus/otuskotlin/marketplace/app/v1/ControllerHelperV1.kt
+++ ok-marketplace-app-ktor/src/main/kotlin/ru/otus/otuskotlin/marketplace/app/v1/ControllerHelperV1.kt
@@ -6,6 +6,7 @@ import io.ktor.server.auth.jwt.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import kotlinx.datetime.Clock
+import ru.otus.otuskotlin.marketplace.api.logs.mapper.toLog
 import ru.otus.otuskotlin.marketplace.api.v1.models.IRequest
 import ru.otus.otuskotlin.marketplace.api.v1.models.IResponse
 import ru.otus.otuskotlin.marketplace.app.mappers.toModel
@@ -13,26 +14,46 @@ import ru.otus.otuskotlin.marketplace.common.MkplContext
 import ru.otus.otuskotlin.marketplace.common.helpers.asMkplError
 import ru.otus.otuskotlin.marketplace.common.models.MkplCommand
 import ru.otus.otuskotlin.marketplace.common.models.MkplState
+import ru.otus.otuskotlin.marketplace.logging.MpLogWrapper
 import ru.otus.otuskotlin.marketplace.mappers.v1.fromTransport
 import ru.otus.otuskotlin.marketplace.mappers.v1.toTransportAd
 
 suspend inline fun <reified Q : IRequest, reified R : IResponse>
-        ApplicationCall.controllerHelperV1(command: MkplCommand? = null, block: MkplContext.() -> Unit) {
+        ApplicationCall.controllerHelperV1(
+    logger: MpLogWrapper,
+    logId: String,
+    command: MkplCommand? = null,
+    crossinline block: suspend MkplContext.() -> Unit
+) {
     val ctx = MkplContext(
         timeStart = Clock.System.now(),
         principal = principal<JWTPrincipal>().toModel(),
     )
     try {
-        val request = receive<Q>()
-        ctx.fromTransport(request)
-        ctx.block()
-        val response = ctx.toTransportAd()
-        respond(response)
+        logger.doWithLogging {
+            val request = receive<Q>()
+            ctx.fromTransport(request)
+            logger.info(
+                msg = "$command request is got",
+                data = ctx.toLog("${logId}-got")
+            )
+            ctx.block()
+            logger.info(
+                msg = "$command request is handled",
+                data = ctx.toLog("${logId}-handled")
+            )
+            val response = ctx.toTransportAd()
+            respond(response)
+        }
     } catch (e: Throwable) {
         command?.also { ctx.command = it }
         ctx.state = MkplState.FAILING
         ctx.errors.add(e.asMkplError())
-        ctx.block()
+        logger.error(
+            msg = "Fail to handle $command request",
+            e = e,
+            data = ctx.toLog("${logId}-error")
+        )
         val response = ctx.toTransportAd()
         respond(response)
     }
--- ok-marketplace-app-ktor/src/main/kotlin/ru/otus/otuskotlin/marketplace/app/v1/rest.kt
+++ ok-marketplace-app-ktor/src/main/kotlin/ru/otus/otuskotlin/marketplace/app/v1/rest.kt
@@ -3,23 +3,25 @@ package ru.otus.otuskotlin.marketplace.app.v1
 import io.ktor.server.application.*
 import io.ktor.server.routing.*
 import ru.otus.otuskotlin.marketplace.backend.services.AdService
+import ru.otus.otuskotlin.marketplace.logging.mpLogger
 
+private val logger = mpLogger(Route::v1Ad::class.java)
 fun Route.v1Ad(service: AdService) {
     route("ad") {
         post("create") {
-            call.createAd(service)
+            call.createAd(service, logger)
         }
         post("read") {
-            call.readAd(service)
+            call.readAd(service, logger)
         }
         post("update") {
-            call.updateAd(service)
+            call.updateAd(service, logger)
         }
         post("delete") {
-            call.deleteAd(service)
+            call.deleteAd(service, logger)
         }
         post("search") {
-            call.searchAd(service)
+            call.searchAd(service, logger)
         }
     }
 }
